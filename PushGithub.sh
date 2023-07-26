29715f0d8ff82cfe2a14d16be0a2936a5dd7db1d
CoolSrj06 <srijanmaurya6602@gmail.com>
2023-07-26 22:55:14 +0530

    Commiting my file Wed Jul 26 22:55:14 IST 2023,

--- PushGithub.sh
+++ PushGithub.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo "Reason for updating"
+read $reason
 git add .
 val=$(date)
-git commit -m "Commiting my file $val"
+git commit -m "Commiting my file $val, $reason"
 git push origin master
